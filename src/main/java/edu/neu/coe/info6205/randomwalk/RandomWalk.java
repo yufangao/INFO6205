1377c44ced3d1ea31da112071642998be24a5456
gaoyufan <gao.yuf@northeastern.edu>
2023-01-30 14:42:25 -0500

    Modify RandomWalk.java

--- src/main/java/edu/neu/coe/info6205/randomwalk/RandomWalk.java
+++ src/main/java/edu/neu/coe/info6205/randomwalk/RandomWalk.java
@@ -20,9 +20,8 @@ public class RandomWalk {
      * @param dy the distance he moves in the y direction
      */
     private void move(int dx, int dy) {
-        // FIXME do move by replacing the following code
-         throw new RuntimeException("Not implemented");
-        // END 
+        this.x += dx;
+        this.y += dy;
     }
 
     /**
@@ -31,8 +30,9 @@ public class RandomWalk {
      * @param m the number of steps the drunkard takes
      */
     private void randomWalk(int m) {
-        // FIXME
-        // END 
+        for (int i = 0; i < m; i++) {
+            randomMove();
+        }
     }
 
     /**
@@ -51,9 +51,7 @@ public class RandomWalk {
      * @return the (Euclidean) distance from the origin to the current position.
      */
     public double distance() {
-        // FIXME by replacing the following code
-         return 0.0;
-        // END 
+        return Math.sqrt(Math.pow(this.x, 2.0) + Math.pow(this.y, 2.0));
     }
 
     /**
@@ -74,13 +72,15 @@ public class RandomWalk {
     }
 
     public static void main(String[] args) {
-        if (args.length == 0)
-            throw new RuntimeException("Syntax: RandomWalk steps [experiments]");
-        int m = Integer.parseInt(args[0]);
-        int n = 30;
-        if (args.length > 1) n = Integer.parseInt(args[1]);
-        double meanDistance = randomWalkMulti(m, n);
-        System.out.println(m + " steps: " + meanDistance + " over " + n + " experiments");
+//        if (args.length == 0)
+//            throw new RuntimeException("Syntax: RandomWalk steps [experiments]");
+        for (int i = 1; i < 21; i++) {
+            //int m = Integer.parseInt(args[0]);
+            int m = i < 10 ? i : i * 2 - 2;
+            int n = 60;
+            if (args.length > 1) n = Integer.parseInt(args[1]);
+            double meanDistance = randomWalkMulti(m, n);
+            System.out.printf("%2d steps: %.4f over %d experiments.\n", m, meanDistance, n);
+        }
     }
-
 }
